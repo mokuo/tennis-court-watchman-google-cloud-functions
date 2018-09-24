2535cac422e4bb9478c303e5a7b1817b172b6d5b
mokuo <tennis10988.yk@gmail.com>
2018-09-24 19:23:17 +0900

    Output available datetime list.

--- index.js
+++ index.js
@@ -1,13 +1,68 @@
 const puppeteer = require('puppeteer');
 
 exports.watchShinjuku = async (req, res) => {
-  const browser = await puppeteer.launch();
+  let navigationPromise;
+
+  const browser = await puppeteer.launch({ defaultViewport: { width: 800, height: 1000 } });
   const page = await browser.newPage();
   await page.goto('https://yoyaku.cultos-y.jp/regasu-shinjuku/reserve/gin_menu');
 
-  await page.waitForSelector('#header h1 img');
-  const pageHeader = await page.$eval('#header h1 img', el => el.getAttribute('title'));
-  console.log(pageHeader);
+  navigationPromise = page.waitForNavigation();
+  await page.click('#contents ul.double li.first input[title="かんたん操作"]');
+  await navigationPromise;
+
+  navigationPromise = page.waitForNavigation();
+  await page.click('#contents ul.double li.first input[title="空き状況確認"]');
+  await navigationPromise;
+
+  navigationPromise = page.waitForNavigation();
+  await page.click('#inner-contents a[title="屋外スポーツ施設"]');
+  await navigationPromise;
+
+  navigationPromise = page.waitForNavigation();
+  await page.click('#inner-contents tbody td.left a[title="西落合公園"]');
+  await navigationPromise;
+
+  const selectorPromise = page.waitForSelector('#inner-contents ul.double-text-buttons a[title="テニス"]');
+  await page.$eval('#inner-contents ul.double-text-buttons2 a[title="西落合公園庭球場"]', el => el.click());
+  await selectorPromise;
+
+  navigationPromise = page.waitForNavigation();
+  await page.click('#inner-contents ul.double-text-buttons a[title="テニス"]');
+  await navigationPromise;
+
+  navigationPromise = page.waitForNavigation();
+  await page.click('#contents #buttons-navigation input#btnOK');
+  await navigationPromise;
+
+  navigationPromise = page.waitForNavigation();
+  await page.click('#buttons-navigation ul.triple li.first a');
+  await navigationPromise;
+
+  const availableDateTimeObj = await page.$eval('#contents #inner-contents1 #timetable .wrapper table', (tableElement) => {
+    const thElements = Array.from(tableElement.querySelectorAll('thead tr th')).slice(1);
+    const timeArray = thElements.map(el => el.textContent.replace(/(\n|\t|<br>|<span>|<\/span>)/g, ''));
+    const obj = {};
+
+    tableElement.querySelectorAll('tbody').forEach((tbodyElement) => {
+      const date = tbodyElement.querySelector('th').textContent;
+      const availableTimeList = [];
+
+      tbodyElement.querySelectorAll('td').forEach((tdElement, index) => {
+        const imgElement = tdElement.querySelector('img');
+        const OX = imgElement.getAttribute('title');
+        if (OX === 'O') {
+          availableTimeList.push(timeArray[index]);
+        }
+      });
+
+      obj[date] = availableTimeList;
+    });
+
+    return obj;
+  });
+
+  console.log(availableDateTimeObj);
 
   await browser.close();
 };
