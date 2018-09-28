e6e1e7f1787ffac1cac667be6c32567a48cc919a
mokuo <tennis10988.yk@gmail.com>
2018-09-29 00:26:44 +0900

    add "落合中央公園".

--- index.js
+++ index.js
@@ -73,25 +73,33 @@ const perform = async (page, parkName) => {
   notifyInfomation(info, parkName);
 };
 
-exports.watchShinjuku = async (req, res) => {
-  const PARK_NAME = '西落合公園';
-
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
+const watchPark = async (browser, parkName) => {
+  const context = await browser.createIncognitoBrowserContext();
+  const page = await context.newPage();
   await page.goto('https://yoyaku.cultos-y.jp/regasu-shinjuku/reserve/gin_menu');
-
   await clickAndWait(page, '#contents ul.double li.first input[title="かんたん操作"]');
   await clickAndWait(page, '#contents ul.double li.first input[title="空き状況確認"]');
   await clickAndWait(page, '#inner-contents a[title="屋外スポーツ施設"]');
-  await clickAndWait(page, `#inner-contents tbody td.left a[title="${PARK_NAME}"]`);
+  await clickAndWait(page, `#inner-contents tbody td.left a[title="${parkName}"]`);
   const tennisSelector = '#inner-contents ul.double-text-buttons a[title="テニス"]';
-  await evalClickAndWait(page, `#inner-contents ul.double-text-buttons2 a[title="${PARK_NAME}庭球場"]`, tennisSelector);
+  await evalClickAndWait(page, `#inner-contents ul.double-text-buttons2 a[title="${parkName}庭球場"]`, tennisSelector);
   await clickAndWait(page, tennisSelector);
   await clickAndWait(page, '#contents #buttons-navigation input#btnOK');
   await clickAndWait(page, '#buttons-navigation ul.triple li.first a');
-  await perform(page, PARK_NAME);
+  await perform(page, parkName);
   await clickAndWait(page, '#timetable .top-nav input[title="次月"]');
-  await perform(page, PARK_NAME);
+  await perform(page, parkName);
+};
 
+exports.watchShinjuku = async (req, res) => {
+  const PARK_NAMES = [
+    '落合中央公園',
+    '西落合公園',
+  ];
+
+  const browser = await puppeteer.launch();
+  await Promise.all(PARK_NAMES.map(async (parkName) => {
+    await watchPark(browser, parkName);
+  }));
   await browser.close();
 };
