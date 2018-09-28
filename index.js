6a3e07b7036cab7d8dd9913bb50ab2c47d815813
mokuo <tennis10988.yk@gmail.com>
2018-09-28 20:52:52 +0900

    add waitAndClick() function.

--- index.js
+++ index.js
@@ -1,45 +1,31 @@
 const puppeteer = require('puppeteer');
 const { WebClient } = require('@slack/client');
 
-exports.watchShinjuku = async (req, res) => {
-  let navigationPromise;
+const waitAndClick = async (page, selector) => {
+  await Promise.all([
+    page.waitForNavigation(),
+    page.click(selector),
+  ]);
+};
 
+exports.watchShinjuku = async (req, res) => {
   const browser = await puppeteer.launch({ defaultViewport: { width: 800, height: 1000 } });
   const page = await browser.newPage();
   await page.goto('https://yoyaku.cultos-y.jp/regasu-shinjuku/reserve/gin_menu');
 
-  navigationPromise = page.waitForNavigation();
-  await page.click('#contents ul.double li.first input[title="かんたん操作"]');
-  await navigationPromise;
-
-  navigationPromise = page.waitForNavigation();
-  await page.click('#contents ul.double li.first input[title="空き状況確認"]');
-  await navigationPromise;
-
-  navigationPromise = page.waitForNavigation();
-  await page.click('#inner-contents a[title="屋外スポーツ施設"]');
-  await navigationPromise;
-
-  navigationPromise = page.waitForNavigation();
-  await page.click('#inner-contents tbody td.left a[title="西落合公園"]');
-  await navigationPromise;
+  await waitAndClick(page, '#contents ul.double li.first input[title="かんたん操作"]');
+  await waitAndClick(page, '#contents ul.double li.first input[title="空き状況確認"]');
+  await waitAndClick(page, '#inner-contents a[title="屋外スポーツ施設"]');
+  await waitAndClick(page, '#inner-contents tbody td.left a[title="西落合公園"]');
 
   const tennisSelector = '#inner-contents ul.double-text-buttons a[title="テニス"]';
   const selectorPromise = page.waitForSelector(tennisSelector);
   await page.$eval('#inner-contents ul.double-text-buttons2 a[title="西落合公園庭球場"]', el => el.click());
   await selectorPromise;
 
-  navigationPromise = page.waitForNavigation();
-  await page.click(tennisSelector);
-  await navigationPromise;
-
-  navigationPromise = page.waitForNavigation();
-  await page.click('#contents #buttons-navigation input#btnOK');
-  await navigationPromise;
-
-  navigationPromise = page.waitForNavigation();
-  await page.click('#buttons-navigation ul.triple li.first a');
-  await navigationPromise;
+  await waitAndClick(page, tennisSelector);
+  await waitAndClick(page, '#contents #buttons-navigation input#btnOK');
+  await waitAndClick(page, '#buttons-navigation ul.triple li.first a');
 
   const availableDateTimeObj = await page.$eval('#contents #inner-contents1 #timetable .wrapper table', (tableElement) => {
     const thElements = Array.from(tableElement.querySelectorAll('thead tr th')).slice(1);
