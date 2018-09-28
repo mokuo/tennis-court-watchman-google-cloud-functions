6b7038c6bf0ce3e943e42f1c804187beeb6c1b0c
mokuo <tennis10988.yk@gmail.com>
2018-09-28 22:17:28 +0900

    add notifyInfo() function.

--- index.js
+++ index.js
@@ -16,21 +16,7 @@ const evalClickAndWait = async (page, selector, nextSelector) => {
   ]);
 };
 
-exports.watchShinjuku = async (req, res) => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto('https://yoyaku.cultos-y.jp/regasu-shinjuku/reserve/gin_menu');
-
-  await clickAndWait(page, '#contents ul.double li.first input[title="かんたん操作"]');
-  await clickAndWait(page, '#contents ul.double li.first input[title="空き状況確認"]');
-  await clickAndWait(page, '#inner-contents a[title="屋外スポーツ施設"]');
-  await clickAndWait(page, '#inner-contents tbody td.left a[title="西落合公園"]');
-  const tennisSelector = '#inner-contents ul.double-text-buttons a[title="テニス"]';
-  await evalClickAndWait(page, '#inner-contents ul.double-text-buttons2 a[title="西落合公園庭球場"]', tennisSelector);
-  await clickAndWait(page, tennisSelector);
-  await clickAndWait(page, '#contents #buttons-navigation input#btnOK');
-  await clickAndWait(page, '#buttons-navigation ul.triple li.first a');
-
+const notifyInfo = async (page, parkName) => {
   const availableDateTimeObj = await page.$eval('#contents #inner-contents1 #timetable .wrapper table', (tableElement) => {
     const thElements = Array.from(tableElement.querySelectorAll('thead tr th')).slice(1);
     const times = thElements.map(el => el.textContent.replace(/(\n|\t|<br>|<span>|<\/span>)/g, ''));
@@ -55,8 +41,6 @@ exports.watchShinjuku = async (req, res) => {
     return obj;
   });
 
-  await browser.close();
-
   let info = '';
   Object.keys(availableDateTimeObj).forEach((key) => {
     const times = availableDateTimeObj[key];
@@ -67,9 +51,30 @@ exports.watchShinjuku = async (req, res) => {
       info += `  - ${time}\n`;
     });
   });
-  const text = (info === '') ? '西落合中央公園 : no available time.' : `西落合公園\n\`\`\`\n${info}\`\`\``;
+  const text = (info === '') ? `${parkName} : no available time.` : `${parkName}\n\`\`\`\n${info}\`\`\``;
 
   const token = process.env.SLACK_TOKEN;
   const web = new WebClient(token);
   web.chat.postMessage({ channel: 'CD1M8BUM7', text });
+};
+
+exports.watchShinjuku = async (req, res) => {
+  const PARK_NAME = '西落合公園';
+
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://yoyaku.cultos-y.jp/regasu-shinjuku/reserve/gin_menu');
+
+  await clickAndWait(page, '#contents ul.double li.first input[title="かんたん操作"]');
+  await clickAndWait(page, '#contents ul.double li.first input[title="空き状況確認"]');
+  await clickAndWait(page, '#inner-contents a[title="屋外スポーツ施設"]');
+  await clickAndWait(page, `#inner-contents tbody td.left a[title="${PARK_NAME}"]`);
+  const tennisSelector = '#inner-contents ul.double-text-buttons a[title="テニス"]';
+  await evalClickAndWait(page, `#inner-contents ul.double-text-buttons2 a[title="${PARK_NAME}庭球場"]`, tennisSelector);
+  await clickAndWait(page, tennisSelector);
+  await clickAndWait(page, '#contents #buttons-navigation input#btnOK');
+  await clickAndWait(page, '#buttons-navigation ul.triple li.first a');
+  await notifyInfo(page, PARK_NAME);
+
+  await browser.close();
 };
