2b3c7de27ffeab9a414158bd1635ab24420bce98
mokuo <tennis10988.yk@gmail.com>
2018-09-30 19:51:12 +0900

    add ResponseMock.

--- local.js
+++ local.js
@@ -1,3 +1,9 @@
 const functions = require('./index');
 
-functions.watchShinjuku();
+class ResponseMock {
+  send(str) { // eslint-disable-line class-methods-use-this
+    console.log(`mock send() : ${str}`); // eslint-disable-line no-console
+  }
+}
+
+functions.watchShinjuku(null, new ResponseMock());
