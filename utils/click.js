cbffd869600ef75da441c6cb941c24aea34e12bc
mokuo <tennis10988.yk@gmail.com>
2019-01-01 13:34:40 +0900

    セミコロン削除

--- utils/click.js
+++ utils/click.js
@@ -2,13 +2,16 @@ const clickAndWait = async (page, selector) => {
   await Promise.all([
     page.waitForNavigation(),
     page.click(selector),
-  ]);
-};
+  ])
+}
 
 const evalClickAndWait = async (page, selector, nextSelector) => {
   await Promise.all([
     page.waitForNavigation(),
     page.$eval(selector, el => el.click()),
     page.waitForSelector(nextSelector),
-  ]);
-};
+  ])
+}
+
+module.exports = clickAndWait
+module.exports = evalClickAndWait
